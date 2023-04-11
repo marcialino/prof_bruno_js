dd7468afaff972d0593a825cae1f3b63934bb8f3
marcialino <marcialino100@gmail.com>
2023-04-11 14:40:16 -0300

    criei typeof

--- bitwise/operador_ternario/tern.js
+++ bitwise/operador_ternario/tern.js
@@ -2,6 +2,8 @@ let num = 10
 
 let res = num%2
 
-if(res==0{
+if (res ==0){
     console.log('PAR')
-})
+}else{
+    console.log('IMPAR')
+}
---
+++ bitwise/operador_ternario/typof.js
@@ -0,0 +1,11 @@
+// TYPEOF OPERADOR UNÁRIO
+
+var n1 = 10
+var n2 = '10'
+var n3 = n1 === n2
+var n4 = {nome:"Márcia"}
+
+console.log("Valor: " + n1 + " Tipo: " + typeof(n1))
+ console.log("Valor: " + n2 + " Tipo: " + typeof(n2))
+ console.log("Valor: " +  n3 + " Tipo: " + typeof(n3))
+ console.log("Valor: " + n4 + " Tipo: " + typeof(n4))
