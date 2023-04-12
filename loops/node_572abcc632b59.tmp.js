a418828bcaf4d1a362cf6a091f6f9f91f6ad777d
marcialino <marcialino100@gmail.com>
2023-04-12 17:14:55 -0300

    Criei dowhile

---
+++ loops/node_572abcc632b59.tmp.js
@@ -0,0 +1,9 @@
+// While = a primeira coisa que se faz é o teste lógico. Se a expressão retornar true, executa os blocos. (ex: enquanto n > 0, faça). Se ela retornar false, não executa os blocos. Neste, você não tem a garantia de que os blocos de comando serão executados.
+
+// Do While = Primeiro você faz o bloco de comando, para depois fazer o teste lógico. Se for falso, já executou pelo menos uma vez, a aí sai do loop e continua o programa. Neste, você tem a garantia que os blocos de comandos serão executados pelo menos uma vez.
+
+let n = 10
+while (n<=10){
+    console.log('Márcia Lino')
+}
+console.log('Fim do Programa!')
