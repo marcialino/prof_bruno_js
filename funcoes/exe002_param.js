44ec96c4b540b84ac66cbec33b4409bb4b61e328
marcialino <marcialino100@gmail.com>
2023-04-13 17:08:15 -0300

    atualizei exe002

--- funcoes/exe002_param.js
+++ funcoes/exe002_param.js
@@ -9,7 +9,15 @@ nome(5,4) // precisa passar dois parâmetros para apresentar um resultado. Senã
 //Você pode também definir valores padrões. Caso você não passe nenhum valor, o sistema assume que é o valor padrão. No exemplo abaixo o valor padrão é zero.
 
 function valor (n3=0,n4=0){
-    console.log(n3*n4)
+    console.log(n3*n4)       /*neste caso está realizando a operação dentro da função. Mas, pode também fazer usando o return*/
 }
+    valor(2,5)
 
-valor(2,5)
+//Com função return
+
+function valores(n5=0, n6=0){
+    var res = n5/n6
+    return res
+}
+ var num = valores(6,2)
+console.log(num)
