9c8889fe6f28ca07b13d712dac5561a4531bce98
marcialino <marcialino100@gmail.com>
2023-04-13 10:57:22 -0300

    crei exe001

---
+++ funcoes/exe001.js
@@ -0,0 +1,12 @@
+/*
+Funções: bloco de comando que pode ser executado, num momento oportuno;
+function nome (){}  - declaração da função
+
+nome()  - chamada da função
+
+*/ 
+
+function nome (){
+    console.log('Márcia Lino')
+}
+nome()
